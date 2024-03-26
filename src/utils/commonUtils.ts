d2e0c909da353509db77c8434f250a234a756495
reshmarevi17 <72592634+reshmarevi17@users.noreply.github.com>
2024-03-26 11:15:44 +0530

    fix: Add month logic to generateElapsedTimeText function

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -25,21 +25,24 @@ type FetchParams = {
 };
 
 export const generateElapsedTimeText = (timeString: string) => {
-  const currentTime = Date.now();
+  const currentTime = new Date().getTime();
   const targetTime = new Date(timeString).getTime();
-  const timeDiff = currentTime - targetTime;
+  const millisecondsDiff = currentTime - targetTime;
 
-  const seconds = Math.floor(timeDiff / 1000);
+  const seconds = Math.floor(millisecondsDiff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
   const years = Math.floor(days / 365);
 
-  if (timeDiff < 60000) return "Just now";
+  if (millisecondsDiff < 60000) return "Just now";
   else if (minutes < 60)
     return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
   else if (hours < 24) return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
-  else if (days < 365) return days === 1 ? "1 day ago" : `${days} days ago`;
+  else if (days < 30) return days === 1 ? "1 day ago" : `${days} days ago`;
+  else if (months < 12)
+    return months === 1 ? "1 month ago" : `${months} months ago`;
   else return years === 1 ? "1 year ago" : `${years} years ago`;
 };
 
