5e855f44f2bd70608ed4b48f6ecf17085de54fd0
reshmarevi17 <72592634+reshmarevi17@users.noreply.github.com>
2024-05-02 17:27:39 +0530

    fix: Style fixes

--- src/components/CloseIcon.tsx
+++ src/components/CloseIcon.tsx
@@ -16,7 +16,7 @@ const CloseIcon: FC<IconProps> = ({ color = '#98A2B3', size= 20 }) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M5 15L15 5M5 5L15 15"
+        d="M1 11L11 1M1 1L11 11"
         stroke={color}
         strokeWidth="2"
         strokeLinecap="round"
