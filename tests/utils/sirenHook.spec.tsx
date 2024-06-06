1cb49b0741c4bfd0d81f548b5ccd97e0919cce2a
reshmarevi17 <reshma.r@keyvalue.systems>
2024-06-06 10:35:34 +0530

    test: test update

--- tests/utils/sirenHook.spec.tsx
+++ tests/utils/sirenHook.spec.tsx
@@ -169,10 +169,10 @@ describe("useSiren hook", () => {
 
     const { markAsReadByDate } = useSiren();
     const untilDate = "2024-02-28T00:00:00Z";
-    const response = await markAsReadByDate(untilDate);
+    const response = await markAsReadByDate({startDate: untilDate});
 
     expect(mockSirenCore.markAsReadByDate).toHaveBeenCalledWith(
-      untilDate
+      {startDate: untilDate}
     );
 
     expect(response).toEqual(ActionResponse);
@@ -188,9 +188,10 @@ describe("useSiren hook", () => {
     });
 
     const { markAsReadByDate } = useSiren();
-    const response = await markAsReadByDate(
-      undefined as unknown as string
-    );
+    const response = await markAsReadByDate({
+      startDate: undefined as unknown as string,
+      isRead: undefined,
+    });
 
     expect(response).toEqual({
       error: errorMap.SIREN_OBJECT_NOT_FOUND,
@@ -239,10 +240,10 @@ describe("useSiren hook", () => {
 
     const { deleteByDate } = useSiren();
     const untilDate = "2024-02-28T00:00:00Z";
-    const response = await deleteByDate(untilDate);
+    const response = await deleteByDate({startDate: untilDate});
 
     expect(mockSirenCore.deleteByDate).toHaveBeenCalledWith(
-      untilDate
+      {startDate: untilDate}
     );
 
     expect(response).toEqual(ActionResponse);
